bf871e6beefa486a5fce330ce0d58343be3e4d55
Jacob Martin <jacobmartin@rccl.com>
2018-05-06 14:11:22 -0400

    Initial Commit

---
+++ PlaygroundHelper/PlaygroundHelper.h
@@ -0,0 +1,19 @@
+//
+//  PlaygroundHelper.h
+//  PlaygroundHelper
+//
+//  Created by Jacob Martin on 5/6/18.
+//  Copyright © 2018 jjm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PlaygroundHelper.
+FOUNDATION_EXPORT double PlaygroundHelperVersionNumber;
+
+//! Project version string for PlaygroundHelper.
+FOUNDATION_EXPORT const unsigned char PlaygroundHelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PlaygroundHelper/PublicHeader.h>
+
+
